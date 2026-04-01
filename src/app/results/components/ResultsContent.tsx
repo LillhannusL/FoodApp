e6e5ea6dc68801b259bb703befc854171e33be3b
Lisa-Ni <niklasson.lisa@gmail.com>
2026-04-01 12:35:05 +0200

    fix-error fixes

--- src/app/results/components/ResultsContent.tsx
+++ src/app/results/components/ResultsContent.tsx
@@ -1,126 +1,136 @@
-"use client";
-export const dynamic = "force-dynamic";
+'use client';
+export const dynamic = 'force-dynamic';
 
-import { useState, useEffect } from "react";
-import { useSearchParams } from "next/navigation";
-import ContentBox from "@/components/shared/ContentBox";
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
+import ContentBox from '@/components/shared/ContentBox';
 import {
-  fetchRandomRecipes,
-  fetchRecipeByIngredient,
-  fetchRecipefromQuiz,
-} from "@/services/recipeApi";
-import RecipeCarousel from "@/components/recipe/recipeCarousel";
-import { useIngredientsStore } from "@/store/useIngredientsStore";
-import { useRouter } from "next/navigation";
-import { useQuizStore } from "@/store/useQuizStore";
-import type { RecipeData } from "@/types";
-import { useResultStore } from "@/store/useResultStore";
+	fetchRandomRecipes,
+	fetchRecipeByIngredient,
+	fetchRecipefromQuiz,
+} from '@/services/recipeApi';
+import RecipeCarousel from '@/components/recipe/recipeCarousel';
+import { useIngredientsStore } from '@/store/useIngredientsStore';
+import { useRouter } from 'next/navigation';
+import { useQuizStore } from '@/store/useQuizStore';
+import type { RecipeData } from '@/types';
+import { useResultStore } from '@/store/useResultStore';
 
 export default function ResultsPage() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const [recipes, setRecipes] = useState<RecipeData[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [isMock, setIsMock] = useState(false);
+	const router = useRouter();
+	const searchParams = useSearchParams();
+	const [recipes, setRecipes] = useState<RecipeData[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [currentIndex, setCurrentIndex] = useState(0);
+	const [isMock, setIsMock] = useState(false);
+	const [error, setError] = useState<string | null>(null);
 
-  const type = searchParams.get("type");
-  const { ingredients } = useIngredientsStore();
-  const { storedResult, setStoredResult } = useResultStore();
-  const { result } = useQuizStore();
+	const type = searchParams.get('type');
+	const { ingredients } = useIngredientsStore();
+	const { storedResult, setStoredResult } = useResultStore();
+	const { result } = useQuizStore();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (!type) {
-        setIsLoading(false);
-        return;
-      }
+	useEffect(() => {
+		const fetchData = async () => {
+			if (!type) {
+				setIsLoading(false);
+				setError('No type found');
+				return;
+			}
 
-      if (storedResult && storedResult.length > 0) {
-        setRecipes(storedResult);
-        setIsLoading(false);
-        return;
-      }
+			if (storedResult && storedResult.length > 0) {
+				setRecipes(storedResult);
+				setIsLoading(false);
+				return;
+			}
 
-      if (type === "search" && (!ingredients || ingredients.length === 0)) {
-        setIsLoading(false);
-        return;
-      }
+			if (type === 'search' && (!ingredients || ingredients.length === 0)) {
+				setIsLoading(false);
+				setError('No recipes found based on the ingredients');
+				return;
+			}
 
-      if (type === "quiz" && !result) {
-        setIsLoading(false);
-        return;
-      }
+			if (type === 'quiz' && !result) {
+				setIsLoading(false);
+				return;
+			}
 
-      setIsLoading(true);
-      try {
-        let data;
+			setIsLoading(true);
+			try {
+				let data;
 
-        if (type === "random") {
-          data = await fetchRandomRecipes();
-        } else if (type === "search") {
-          data = await fetchRecipeByIngredient(ingredients);
-        } else {
-          data = await fetchRecipefromQuiz(result);
-        }
+				if (type === 'random') {
+					data = await fetchRandomRecipes();
+				} else if (type === 'search') {
+					data = await fetchRecipeByIngredient(ingredients);
+				} else {
+					data = await fetchRecipefromQuiz(result);
+				}
 
-        setRecipes(data);
-        setStoredResult(data);
-      } catch (error) {
-        setIsMock(true);
-        console.error("Fetching error:", error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+				setRecipes(data);
+				setStoredResult(data);
+			} catch (err) {
+				setIsMock(true);
+				setError('An error occured');
+				console.error('Fetching error:', err);
+			} finally {
+				setIsLoading(false);
+			}
+		};
 
-    fetchData();
-  }, [type, ingredients, result, setStoredResult]);
+		fetchData();
+	}, [type, ingredients, result, setStoredResult]);
 
-  const handleClick = () => {
-    if (!recipes[currentIndex]) return;
-    router.push(`/recipes/${recipes[currentIndex].id}`);
-  };
+	const handleClick = () => {
+		if (!recipes[currentIndex]) return;
+		router.push(`/recipes/${recipes[currentIndex].id}`);
+	};
 
-  if (!type) {
-    return null;
-  }
+	if (!type) {
+		return null;
+	}
 
-  return (
-    <section className="px-4 flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold pt-16 pb-8 text-center md:pt-28">
-        We found these:
-      </h1>
+	return (
+		<section className="px-4 flex flex-col items-center justify-center">
+			<h1 className="text-4xl font-bold pt-16 pb-8 text-center md:pt-28">
+				We found these:
+			</h1>
 
-      <ContentBox>
-        {isLoading && (
-          <div className="w-full flex justify-center items-center flex-col gap-4">
-            <div className="skeleton h-96 w-full"></div>
-            <div className="skeleton h-4 w-28"></div>
-            <div className="skeleton h-4 w-full"></div>
-            <div className="skeleton h-4 w-full"></div>
-          </div>
-        )}
+			<ContentBox>
+				{isLoading && (
+					<div className="w-full flex justify-center items-center flex-col gap-4">
+						<div className="skeleton h-96 w-full"></div>
+						<div className="skeleton h-4 w-28"></div>
+						<div className="skeleton h-4 w-full"></div>
+						<div className="skeleton h-4 w-full"></div>
+					</div>
+				)}
 
-        {isMock && (
-          <div className="badge badge-error gap-2 p-4 mb-4">
-            Demo mode: API Limit is reached
-          </div>
-        )}
-        <div>
-          <RecipeCarousel
-            recipes={recipes}
-            currentIndex={currentIndex}
-            setCurrentIndex={setCurrentIndex}
-          />
-        </div>
-        <button
-          className="btn btn-block bg-green-gradient shadow-md border-none text-lg text-light-primary py-4 font-semibold rounded-xl sm:max-w-xl mx-auto"
-          onClick={handleClick}
-        >
-          See recipe
-        </button>
-      </ContentBox>
-    </section>
-  );
+				{error && (
+					<p className="text-error text-lg text-center mt-24">{error}</p>
+				)}
+
+				{isMock && (
+					<div className="badge badge-error gap-2 p-4 mb-4">
+						Demo mode: API Limit is reached
+					</div>
+				)}
+				<div>
+					<RecipeCarousel
+						recipes={recipes}
+						currentIndex={currentIndex}
+						setCurrentIndex={setCurrentIndex}
+					/>
+				</div>
+				{!error && (
+					<button
+						className="btn btn-block bg-green-gradient shadow-md border-none text-lg text-light-primary py-4 font-semibold rounded-xl sm:max-w-xl mx-auto"
+						onClick={handleClick}
+					>
+						See recipe
+					</button>
+				)}
+			</ContentBox>
+		</section>
+	);
 }
--- src/components/shared/SearchFunction.tsx
+++ src/components/shared/SearchFunction.tsx
@@ -1,99 +1,125 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import { useIngredientsStore } from "@/store/useIngredientsStore";
-import { useRouter } from "next/navigation";
-import { useResultStore } from "@/store/useResultStore";
+import { useState } from 'react';
+import { useIngredientsStore } from '@/store/useIngredientsStore';
+import { useRouter } from 'next/navigation';
+import { useResultStore } from '@/store/useResultStore';
 
 export default function SearchFunction() {
-  const router = useRouter();
-  const [inputValue, setInputValue] = useState("");
-  const [ingredients, setIngredients] = useState<string[]>([]);
-  const { setStoreIngredients } = useIngredientsStore();
-  const { clearStoredResult } = useResultStore();
+	const router = useRouter();
+	const [inputValue, setInputValue] = useState('');
+	const [ingredients, setIngredients] = useState<string[]>([]);
+	const { setStoreIngredients } = useIngredientsStore();
+	const { clearStoredResult } = useResultStore();
+	const [error, setError] = useState<string | null>(null);
 
-  const colors = ["bg-green-gradient", "bg-red-gradient", "bg-orange-gradient"];
+	const colors = ['bg-green-gradient', 'bg-red-gradient', 'bg-orange-gradient'];
 
-  const addIngredient = () => {
-    if (inputValue.trim() === "") return;
-    setIngredients((prev) => [...prev, inputValue]);
-    setInputValue("");
-  };
+	const validateSearch = (inputValue: string) => {
+		const trimmed = inputValue.trim();
 
-  const deleteItem = (indexClicked: number) => {
-    setIngredients(ingredients.filter((_, index) => index !== indexClicked));
-  };
+		if (!trimmed) {
+			setError('You need to write something');
+			return null;
+		}
 
-  const handleSearchClick = () => {
-    setStoreIngredients(ingredients);
-    clearStoredResult();
-    router.push("/results?type=search");
-  };
+		const cleanInput = trimmed.replace(/[^a-zA-Z0-9åäöÅÄÖ\s]/g, '');
 
-  return (
-    <div className="flex flex-col justify-center items-center w-full">
-      <label className="input input-bordered flex items-center gap-4 rounded-full h-18 bg-light-primary/60 backdrop-blur-md shadow-inner focus-within:outline-none sm:w-full sm:max-w-4xl border-none">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="35"
-          height="35"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="lucide lucide-search-icon lucide-search"
-        >
-          <path d="m21 21-4.34-4.34" />
-          <circle cx="11" cy="11" r="8" />
-        </svg>
-        <input
-          type="search"
-          placeholder="tomato"
-          className="grow bg-transparent focus:outline-none"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") addIngredient();
-          }}
-        />
-        <button
-          className="btn btn-circle border-none bg-blue-gradient text-xl hover:scale-110 transition-transform"
-          onClick={addIngredient}
-        >
-          +
-        </button>
-      </label>
+		if (cleanInput.length < 2) {
+			setError('Must be at least 2 letters');
+			return null;
+		}
 
-      <div className="mt-4">
-        <ul className="flex flex-wrap gap-2">
-          {ingredients.map((ingredient, index) => (
-            <li
-              key={index}
-              className={`badge badge-lg gap-2 p-4 rounded-2xl bg-white/10 backdrop-blur-md border-none text-light-primary shadow-md ${colors[index % 3]}`}
-            >
-              <span>{ingredient}</span>
-              <button
-                className="hover:text-error transition-colors font-bold ml-1"
-                onClick={() => deleteItem(index)}
-              >
-                X
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="w-full flex justify-center pt-10">
-        {ingredients.length > 0 && (
-          <button
-            className="btn btn-xl btn-block border-none text-xl bg-orange-800/90 rounded-xl shadow-md mt-4 sm:max-w-2xl"
-            onClick={handleSearchClick}
-          >
-            Search recipe
-          </button>
-        )}
-      </div>
-    </div>
-  );
+		setError(null);
+		return cleanInput;
+	};
+
+	const addIngredient = () => {
+		const cleanInput = validateSearch(inputValue);
+		if (!cleanInput) return;
+		setIngredients((prev) => [...prev, cleanInput]);
+		setInputValue('');
+	};
+
+	const deleteItem = (indexClicked: number) => {
+		setIngredients(ingredients.filter((_, index) => index !== indexClicked));
+	};
+
+	const handleSearchClick = () => {
+		setStoreIngredients(ingredients);
+		clearStoredResult();
+		router.push('/results?type=search');
+	};
+
+	return (
+		<div className="flex flex-col justify-center items-center w-full">
+			<label className="input input-bordered flex items-center gap-4 rounded-full h-18 bg-light-primary/60 backdrop-blur-md shadow-inner focus-within:outline-none sm:w-full sm:max-w-4xl border-none">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="35"
+					height="35"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					strokeWidth="2"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					className="lucide lucide-search-icon lucide-search"
+				>
+					<path d="m21 21-4.34-4.34" />
+					<circle cx="11" cy="11" r="8" />
+				</svg>
+				<input
+					type="search"
+					placeholder="tomato"
+					className="grow bg-transparent focus:outline-none"
+					value={inputValue}
+					onChange={(e) => {
+						setInputValue(e.target.value);
+						if (error) setError(null);
+					}}
+					onKeyDown={(e) => {
+						if (e.key === 'Enter') addIngredient();
+					}}
+				/>
+				<button
+					className="btn btn-circle border-none bg-blue-gradient text-xl hover:scale-110 transition-transform"
+					onClick={addIngredient}
+				>
+					+
+				</button>
+			</label>
+
+			{error && <p className="text-error text-sm mt-1 ml-1">{error}</p>}
+
+			<div className="mt-4">
+				<ul className="flex flex-wrap gap-2">
+					{ingredients.map((ingredient, index) => (
+						<li
+							key={index}
+							className={`badge badge-lg gap-2 p-4 rounded-2xl bg-white/10 backdrop-blur-md border-none text-light-primary shadow-md ${colors[index % 3]}`}
+						>
+							<span>{ingredient}</span>
+							<button
+								className="hover:text-error transition-colors font-bold ml-1"
+								onClick={() => deleteItem(index)}
+							>
+								X
+							</button>
+						</li>
+					))}
+				</ul>
+			</div>
+			<div className="w-full flex justify-center pt-10">
+				{ingredients.length > 0 && (
+					<button
+						className="btn btn-xl btn-block border-none text-xl bg-orange-800/90 rounded-xl shadow-md mt-4 sm:max-w-2xl"
+						onClick={handleSearchClick}
+					>
+						Search recipe
+					</button>
+				)}
+			</div>
+		</div>
+	);
 }
